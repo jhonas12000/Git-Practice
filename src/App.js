ace907ee56a3f97d9d9c067f044aa8b7746ee8b5
Yonas <ytwmichael@gmail.com>
2021-09-17 16:30:21 -0700

    Photo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <h1>Git-Practice</h1>
       <p>Practice how to add, commit and puch into github</p>
+      <img src='images/massawa.jpeg' alt='massawa-vacation'/>
     </div>
   );
 }
