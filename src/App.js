aa9793f1b77ba755449ebd4117e75e3770646943
Yonas <ytwmichael@gmail.com>
2021-09-17 12:18:05 -0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Git-Practice</h1>
+      <p>Practice how to add, commit and puch into github</p>
+    </div>
+  );
+}
+
+export default App;
