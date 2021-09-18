13b9d9eed81991ace8b55f756f0a32f445fc8554
Yonas <ytwmichael@gmail.com>
2021-09-17 17:01:50 -0700

    components created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import Main from './components/main';
 import './App.css';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Git-Practice</h1>
-      <p>Practice how to add, commit and puch into github</p>
-      <img src='images/massawa.jpeg' alt='massawa-vacation'/>
+      <Main />
     </div>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Main() {
+    return (
+      <div className="container">
+        <div className='row'>  
+            <h1>Git-Practice</h1>
+            <p>Practice how to add, commit and puch into github</p>
+        </div>
+        <div className='row'>
+            <img src='images/massawa.jpeg' alt='massawa-vacation'/>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Main;
